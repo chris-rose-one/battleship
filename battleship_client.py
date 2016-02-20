3f96e65cb353ea4c61594d976db4b10a4924c13e
chris-rose-one <chris.rose.one@gmail.com>
2016-02-20 20:33:30 +1100

    Added files via upload

--- battleship_client.py
+++ battleship_client.py
@@ -4,71 +4,74 @@ import time
 from jsonsocket import *
 from battleship_view import *
 
-opponent_no, player_no, board_space, ships_key = 0, 0, 0, []
-
-def new_game(sock):
-	print('Re-entering the servers game queue')
-	time.sleep(10)
-	encode(sock, {'enter_queue': {}})
-
-def battleship_client():
-	if(len(sys.argv) < 3) :
-		print 'Usage : python battleship_client.py ServerAddress port'
-		sys.exit()
-
-	host, port = sys.argv[1], int(sys.argv[2])
-	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	sock.settimeout(2)
-	socket_list = [sock]
+class Client(object):
 	
-	try: sock.connect((host, port))
-	except: print 'Unable to connect'; sys.exit()
-	print 'Connected to remote host.'
+	opponent_no, player_no, board_space, ships_key = 0, 0, 0, []
 	
-	while 1:
-		ready_to_read,ready_to_write,in_error = select.select(socket_list , [], [])
+	def __init__(self):	
+		if(len(sys.argv) < 3) :
+			print 'Usage : python battleship_client.py ServerAddress PORT'
+			sys.exit()
+
+		HOST, PORT = sys.argv[1], int(sys.argv[2])
+		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		sock.settimeout(2)
+		self.socket_list = [sock]
+	
+		try: sock.connect((HOST, PORT))
+		except: print 'Unable to connect'; sys.exit()
+		print 'Connected to remote host.'
+	
+	def new_game(self, sock):
+		print('Re-entering the servers game queue')
+		time.sleep(10)
+		encode(sock, {'enter_queue': {}})
+
+	def main(self):
+		while 1:
+			ready_to_read,ready_to_write,in_error = select.select(self.socket_list , [], [])
 		 
-		for sock in ready_to_read:
-			data = decode(sock)
-			if data:
-				if 'init_data' in data:
-					opponent_no = data['init_data']['opponent_no']
-					player_no = data['init_data']['player_no']
-					board_space = data['init_data']['board_space']
-					ships_key = data['init_data']['player_ships']
-				if 'battle_data' in data:
-					attacker = data['battle_data']['attacker']
-					target = data['battle_data']['target']
-					attack_result = data['battle_data']['attack_result']
-					opponent_board = data['battle_data']['opponent_board']
-					player_board = data['battle_data']['player_board']
-					for ship in ships_key:
-						for pair in ship['coordinates']:
-							if not (player_board[pair[0]][pair[1]] == 'H' or player_board[pair[0]][pair[1]] == '*'): 
-								player_board[pair[0]][pair[1]] = '0'
-					opponent_fleet_sunk = data['battle_data']['opponent_fleet_sunk']
-					player_fleet_sunk = data['battle_data']['player_fleet_sunk']
-					os.system('cls' if os.name == 'nt' else 'clear')
-					if attacker == player_no: print_attack_result(attack_result, target)
-					elif attacker == opponent_no: print_damage_report(attack_result, target)
-					print_board(opponent_board, board_space)
-					print_board(player_board, board_space)
-					if opponent_fleet_sunk == True: print_success()
-					elif player_fleet_sunk == True: print_defeat()
-					if opponent_fleet_sunk == True or player_fleet_sunk == True:
+			for sock in ready_to_read:
+				data = decode(sock)
+				if data:
+					if 'init_data' in data:
+						opponent_no = data['init_data']['opponent_no']
+						player_no = data['init_data']['player_no']
+						board_space = data['init_data']['board_space']
+						ships_key = data['init_data']['player_ships']
+					if 'battle_data' in data:
+						attacker = data['battle_data']['attacker']
+						target = data['battle_data']['target']
+						attack_result = data['battle_data']['attack_result']
+						opponent_board = data['battle_data']['opponent_board']
+						player_board = data['battle_data']['player_board']
+						for ship in ships_key:
+							for pair in ship['coordinates']:
+								if not (player_board[pair[0]][pair[1]] == 'H' or player_board[pair[0]][pair[1]] == '*'): 
+									player_board[pair[0]][pair[1]] = '0'
+						opponent_fleet_sunk = data['battle_data']['opponent_fleet_sunk']
+						player_fleet_sunk = data['battle_data']['player_fleet_sunk']
+						os.system('cls' if os.name == 'nt' else 'clear')
+						if attacker == player_no: print_attack_result(attack_result, target)
+						elif attacker == opponent_no: print_damage_report(attack_result, target)
+						print_board(opponent_board, board_space)
+						print_board(player_board, board_space)
+						if opponent_fleet_sunk == True: print_success()
+						elif player_fleet_sunk == True: print_defeat()
+						if opponent_fleet_sunk == True or player_fleet_sunk == True:
+							opponent_no, player_no, board_space, ships_key = 0, 0, 0, []
+							new_game(sock)
+					if 'orders_request' in data:
+						if data['orders_request'] == True:
+							target_coordinates = get_admirals_orders()
+							encode(sock, {'orders': {'coordinates': target_coordinates}})
+					if 'opponent_disconnected' in data:
+						print('your opponent disconnected from the server')
 						opponent_no, player_no, board_space, ships_key = 0, 0, 0, []
-						new_game(sock)
-				if 'orders_request' in data:
-					if data['orders_request'] == True:
-						target_coordinates = get_admirals_orders()
-						encode(sock, {'orders': {'coordinates': target_coordinates}})
-				if 'opp_disconnect' in data:
-					print('your opponent disconnected from the server')
-					opponent_no, player_no, board_space, ships_key = 0, 0, 0, []
-					new_game(sock)
-			else:
-				print 'Disconnected from server'
-				sys.exit()
+						self.new_game(sock)
+				else:
+					print 'Disconnected from server'
+					sys.exit()
 
 if __name__ == "__main__":
-	sys.exit(battleship_client())
+	Client().main()
--- battleship_server.py
+++ battleship_server.py
@@ -1,19 +1,13 @@
 import sys
 import select
 from jsonsocket import *
-from battleship_model import *
-
-HOST = socket.gethostbyname(socket.gethostname())
-PORT = 9009
-socket_list = []
-game_queue = []
-live_games = []
+import battleship_model as model
 
 class Online_Game(object):
 	def __init__(self, sock1, sock2):
 		self.board_space = 10
-		self.player1 = Player(self.board_space, 1, sock1)
-		self.player2 = Player(self.board_space, 2, sock2)
+		self.player1 = model.Player(self.board_space, 1, sock1)
+		self.player2 = model.Player(self.board_space, 2, sock2)
 		self.send_init_data(self.player1, self.player2)
 		self.send_init_data(self.player2, self.player1)
 		self.send_battle_data(self.player1, self.player2)
@@ -60,58 +54,65 @@ class Online_Game(object):
 			'orders_request': self.is_it_my_turn(player, opponent),
 		})
 		
-	def player_disconnect(self, sock):
-		if sock == self.player1.connection: encode(self.player2.connection, {'opp_disconnect': {}})
-		elif sock == self.player2.connection: encode(self.player1.connection, {'opp_disconnect': {}})	
+	def player_disconnected(self, sock):
+		if sock == self.player1.connection: encode(self.player2.connection, {'opponent_disconnected': {}})
+		elif sock == self.player2.connection: encode(self.player1.connection, {'opponent_disconnected': {}})	
 
-def get_game_object(sock):
-	for game in live_games:
-		if sock == game.player1.connection or sock == game.player2.connection: return game
-
-def start_game():
-	game = Online_Game(game_queue[0], game_queue[1])
-	live_games.append(game)
-	for i in range(2): game_queue.remove(game_queue[0])
-						
-def battleship_server():
-	server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	server_socket.bind((HOST, PORT))
-	server_socket.listen(10)
-	socket_list.append(server_socket)
-
-	print('battleship server started at %s on port %s' % (HOST, str(PORT)))
-
-	while 1:
-		ready_to_read,ready_to_write,in_error = select.select(socket_list,[],[],0)
-
-		for sock in ready_to_read:
-			if sock == server_socket: 
-				conn, addr = server_socket.accept()
-				socket_list.append(conn)
-				print "Client (%s, %s) connected" % addr
-				game_queue.append(conn)
-				if len(game_queue) >= 2: start_game()
-			else:
-				try:
-					data = decode(sock)
-					if data:
-						if 'orders' in data:
-							game = get_game_object(sock)
-							if game: game.direct_attack(sock, data['orders']['coordinates'])
-						if 'enter_queue' in data:
-							if sock not in game_queue: game_queue.append(sock)
-							if len(game_queue) >= 2: start_game()
-					else:
-						if sock in socket_list: socket_list.remove(sock)
-						if sock in game_queue: game_queue.remove(sock)
-						else: 
-							game = get_game_object(sock)
-							if game: game.player_disconnect(sock); live_games.remove(game)
-						print "Client (%s, %s) disconnected" % sock.getpeername()
-				except: continue
+class Server(object):
 	
-	server_socket.close()
+	HOST = socket.gethostbyname(socket.gethostname())
+	PORT = 9009
+	socket_list = []
+	game_queue = []
+	live_games = []
+	
+	def __init__(self):
+		self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		self.server_socket.bind((self.HOST, self.PORT))
+		self.server_socket.listen(10)
+		self.socket_list.append(self.server_socket)
+		print('battleship server started at %s on port %s' % (self.HOST, str(self.PORT)))
+
+	def get_game_object(self, sock):
+		for game in self.live_games:
+			if sock == game.player1.connection or sock == game.player2.connection: return game
+
+	def start_game(self):
+		game = Online_Game(self.game_queue[0], self.game_queue[1])
+		self.live_games.append(game)
+		for i in range(2): self.game_queue.remove(self.game_queue[0])
+
+	def main(self):
+		while 1:
+			ready_to_read,ready_to_write,in_error = select.select(self.socket_list,[],[],0)
+
+			for sock in ready_to_read:
+				if sock == self.server_socket: 
+					conn, addr = self.server_socket.accept()
+					self.socket_list.append(conn)
+					print "Client (%s, %s) connected" % addr
+					self.game_queue.append(conn)
+					if len(self.game_queue) >= 2: self.start_game()
+				else:
+					try:
+						data = decode(sock)
+						if data:
+							if 'orders' in data:
+								game = self.get_game_object(sock)
+								if game: game.direct_attack(sock, data['orders']['coordinates'])
+							if 'enter_queue' in data:
+								if sock not in self.game_queue: self.game_queue.append(sock)
+								if len(self.game_queue) >= 2: self.start_game()
+						else:
+							if sock in self.socket_list: self.socket_list.remove(sock)
+							if sock in self.game_queue: self.game_queue.remove(sock)
+							else: 
+								game = self.get_game_object(sock)
+								if game: game.player_disconnected(sock); self.live_games.remove(game)
+							print "Client (%s, %s) disconnected" % sock.getpeername()
+					except: continue
+		server_socket.close()
 
 if __name__ == "__main__":
-	sys.exit(battleship_server())
+	Server().main()
