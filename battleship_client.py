d967c1dfc692d7693df9ce947c7f2453f759f082
chris-rose-one <chris.rose.one@gmail.com>
2016-03-17 00:08:15 +1100

    Added files via upload

--- battleship_client.py
+++ battleship_client.py
@@ -1,10 +1,11 @@
 import sys, os, socket, select, time
 from battleship_utils import send_json, receive_json
-import battleship_view as view
+from battleship_view import View
 
 class Client(object):
 	
 	opponent_no, player_no, board_space, ships_key = 0, 0, 0, []
+	view = View(sys.version_info)
 	
 	def __init__(self):	
 		if(len(sys.argv) < 3) :
@@ -28,18 +29,18 @@ class Client(object):
 	def main(self):
 		while 1:
 			ready_to_read,ready_to_write,in_error = select.select(self.socket_list , [], [])
-		 	for sock in ready_to_read:
+			for sock in ready_to_read:
 				data = receive_json(sock)
 				if data:
 					if 'init_data' in data:
 						init_data = data.get('init_data')
-						opponent_no = init_data.get('opponent_no')
-						player_no = init_data.get('player_no')
+						self.opponent_no = init_data.get('opponent_no')
+						self.player_no = init_data.get('player_no')
 						player_board = init_data.get('player_board')
-						ships_key = init_data.get('player_ships')
-						board_space = init_data.get('board_space')
+						self.ships_key = init_data.get('player_ships')
+						self.board_space = init_data.get('board_space')
 						os.system('cls' if os.name == 'nt' else 'clear')
-						view.print_brief(board_space, player_board, ships_key); time.sleep(60)
+						self.view.print_brief(self.board_space, player_board, self.ships_key); time.sleep(60)
 					if 'battle_data' in data:
 						battle_data = data.get('battle_data')
 						attacker = battle_data.get('attacker')
@@ -50,27 +51,27 @@ class Client(object):
 						opponent_fleet_sunk = battle_data.get('opponent_fleet_sunk')
 						player_fleet_sunk = battle_data.get('player_fleet_sunk')
 						os.system('cls' if os.name == 'nt' else 'clear')
-						if attacker == player_no: view.print_attack_result(attack_result, target)
-						elif attacker == opponent_no: view.print_damage_report(attack_result, target)
-						print
-						view.print_board(board_space, opponent_board)
-						view.print_board(board_space, player_board, ships_key)
-						if opponent_fleet_sunk == True: view.print_success()
-						elif player_fleet_sunk == True: view.print_defeat()
+						if attacker == self.player_no: self.view.print_attack_result(attack_result, target)
+						elif attacker == self.opponent_no: self.view.print_damage_report(attack_result, target)
+						print()
+						self.view.print_board(self.board_space, opponent_board)
+						self.view.print_board(self.board_space, player_board, self.ships_key)
+						if opponent_fleet_sunk == True: self.view.print_success()
+						elif player_fleet_sunk == True: self.view.print_defeat()
 						if opponent_fleet_sunk == True or player_fleet_sunk == True:
-							opponent_no, player_no, board_space, ships_key = 0, 0, 0, []
+							self.opponent_no, self.player_no, self.board_space, self.ships_key = 0, 0, 0, []
 							self.new_game(sock)
 					if 'orders_request' in data:
 						if data['orders_request'] == True:
-							target_coordinates = view.get_admirals_orders()
+							target_coordinates = self.view.get_admirals_orders()
 							send_json(sock, {'orders': {'coordinates': target_coordinates}})
 						elif data['orders_request'] == False: print('  ' + 'Opponents turn')
 					if 'opponent_disconnected' in data:
 						print('your opponent disconnected from the server')
-						opponent_no, player_no, board_space, ships_key = 0, 0, 0, []
+						self.opponent_no, self.player_no, self.board_space, self.ships_key = 0, 0, 0, []
 						self.new_game(sock)
 				else:
-					print 'Disconnected from server'
+					print('Disconnected from server')
 					sys.exit()
 
 if __name__ == "__main__":
